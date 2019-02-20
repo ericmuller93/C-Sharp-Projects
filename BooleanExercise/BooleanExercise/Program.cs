f4c79fd0a7f79082fe3d7531bdbcbf36f6c5566c
emuller93 <ericmuller93@gmail.com>
2019-02-20 13:47:11 -0800

    commiting boolean exercise

---
+++ BooleanExercise/BooleanExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BooleanExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Car Insurance Approval Questionnaire!");
+            Console.WriteLine("What is your age?");
+            string ageStr = Console.ReadLine();
+            int age = Convert.ToInt16(ageStr);
+            Console.WriteLine("Have you ever had a DUI? (true or false)");
+            string dui = Console.ReadLine();
+            Console.WriteLine("How many speeding tickets have you had?");
+            string ticketsStr = Console.ReadLine();
+            int tickets = Convert.ToInt16(ticketsStr);
+            bool qualified = (age > 15 && dui == "false" && tickets <= 3);
+            Console.WriteLine("Qualified for Insurace?: "+ qualified);
+            Console.ReadLine();
+        }
+    }
+}
