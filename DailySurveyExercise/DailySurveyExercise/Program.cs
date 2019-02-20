822f21b8534a29e62fb1e0a27479a7d36b321e00
emuller93 <ericmuller93@gmail.com>
2019-02-20 12:56:53 -0800

    recommiting all changes

---
+++ DailySurveyExercise/DailySurveyExercise/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("The Tech Academy");
+        Console.WriteLine("Student Daily Report");
+        Console.WriteLine("What course are you on?");
+        string course = Console.ReadLine();
+        Console.WriteLine("What Page Number?");
+        string pageNum = Console.ReadLine();
+        int intPageNum = Convert.ToInt16(pageNum);
+        Console.WriteLine("Do you need help with anything? Please answer True or False");
+        string helpStr = Console.ReadLine();
+        bool helpBool = Convert.ToBoolean(helpStr);
+        Console.WriteLine("Were there any positive experiences youd like to share? Please give specifics.");
+        string exp = Console.ReadLine();
+        Console.WriteLine("Is there any other feedback youd like to provide? Please be specific.");
+        string feedback = Console.ReadLine();
+        Console.WriteLine("How many hours did you study today?");
+        string hours = Console.ReadLine();
+        int intHours = Convert.ToInt16(hours);
+        Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+        Console.ReadLine();
+    }
+}
---
+++ DataTypesExercise/DataTypesExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter any number!");
+            string firstNum = Console.ReadLine();
+            double firstAnswer = Convert.ToDouble(firstNum) * 50;
+            Console.WriteLine("Your number times 50 is " + firstAnswer);
+            Console.WriteLine("Please enter another number!");
+            string secondNum = Console.ReadLine();
+            double secondAnswer = Convert.ToDouble(secondNum) + 25;
+            Console.WriteLine("Your number plus 25 is " + secondAnswer);
+            Console.WriteLine("Please enter another number!");
+            string thirdNum = Console.ReadLine();
+            double thirdAnswer = Convert.ToDouble(thirdNum) / 12.5;
+            Console.WriteLine("Your number divided by 12.5 is " + thirdAnswer);
+            Console.WriteLine("Please enter another number!");
+            string fourthNum = Console.ReadLine();
+            double fourthNumDub = Convert.ToDouble(fourthNum);
+            bool fourthAnswer = (fourthNumDub > 50);
+            Console.WriteLine("Is your number greater than 50?: " + fourthAnswer);
+            Console.WriteLine("Please enter another number!");
+            string fifthNum = Console.ReadLine();
+            double fifthAnswer = Convert.ToDouble(fifthNum) % 7;
+            Console.WriteLine("The remainder of your number divided by 7 is: " + fifthAnswer);
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
