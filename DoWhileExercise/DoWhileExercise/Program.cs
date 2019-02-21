e8eeb3156320ed0aa95f780d402009645f50e74b
emuller93 <ericmuller93@gmail.com>
2019-02-21 11:29:37 -0800

    Do while Exercise

---
+++ DoWhileExercise/DoWhileExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DoWhileExercise
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter a number!");
+            string number = Console.ReadLine();
+            int num = Convert.ToInt16(number);
+
+            do
+            {
+                if (num < 5) { 
+                    Console.WriteLine("The current number is " + num + ". Let's add 1 to it!");
+                    num++;
+                }
+
+                if (num > 5)
+                {
+                    Console.WriteLine("That number is greater than 5. Try again.");
+                    number = Console.ReadLine();
+                    num = Convert.ToInt16(number);
+                }
+
+                if (num == 5)
+                {
+                    Console.WriteLine("Your number equals 5 now!");
+                    break;
+                }
+            } while (true);
+         Console.ReadLine();
+        }
+    }
+}
