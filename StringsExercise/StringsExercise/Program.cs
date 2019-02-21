c2f1d9b0309fad961afeae968937ee7413a6b8a3
emuller93 <ericmuller93@gmail.com>
2019-02-21 13:15:25 -0800

    added comment

--- StringsExercise/StringsExercise/Program.cs
+++ StringsExercise/StringsExercise/Program.cs
@@ -12,9 +12,9 @@ namespace StringsExercise
         {
             string text1 = "My name is";
             string text2 = "Eric";
-            text2 = text2.ToUpper();
+            text2 = text2.ToUpper(); //this converts text 2 to all upper case
             StringBuilder myParagraph = new StringBuilder("I hope you are doing well today \n");
-            myParagraph.Append(text1 +"\n");
+            myParagraph.Append(text1 +"\n"); //here we start concatnating our strings
             myParagraph.Append(text2 + "\n");
             myParagraph.Append("And I think cheese is tasty.");
             Console.WriteLine(myParagraph);
