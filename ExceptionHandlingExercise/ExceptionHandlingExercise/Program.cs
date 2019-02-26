37662af31c1bd596fbd8f63466a10af1a58ea578
emuller93 <ericmuller93@gmail.com>
2019-02-26 13:13:58 -0800

    commiting exception handling exercises

---
+++ ExceptionHandlingExercise/ExceptionHandlingExercise/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Pick a number. \n");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Pick another number. \n");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Lets divide the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            }
+
+            catch (FormatException) //if a format exception put the below message
+            {
+                Console.WriteLine("Please type a whole number"); // custom error message
+            }
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero!");
+            }
+
+            catch (Exception ex) //need to put ex here if you want the generic error message
+            {
+                Console.WriteLine(ex.Message); // ex.Message will enter your generic error message
+            }
+
+            finally // this is here because it will take place no matter what
+            {       // so we will have this read line no matter errors or not
+                Console.ReadLine();
+            }   
+        }
+    }
---
+++ ExceptionHandlingExercise2/ExceptionHandlingExercise2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+            myList.Add(10);
+            myList.Add(20);
+            myList.Add(30);
+            myList.Add(40);
+            myList.Add(50);
+            Console.WriteLine("Please enter an integer to divide by the numbers in my list"); 
+            try
+            {
+                int userNumber = Convert.ToInt16(Console.ReadLine());
+                for (int i = 0; i < myList.Count; i++)
+                {
+                    int solution = myList[i] / userNumber;
+                    Console.WriteLine(solution);
+                }
+            }
+
+            catch (FormatException) //if a format exception put the below message
+            {
+                Console.WriteLine("Please type a whole number"); // custom error message
+            }
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero!");
+            }
+
+            catch (Exception ex) //need to put ex here if you want the generic error message
+            {
+                Console.WriteLine(ex.Message); // ex.Message will enter your generic error message
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
+        }
+    }
