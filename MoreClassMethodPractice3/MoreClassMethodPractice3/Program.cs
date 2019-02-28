49ddfb2062a753a124664a270feddca707f48314
emuller93 <ericmuller93@gmail.com>
2019-02-28 13:55:06 -0800

    update

--- MoreClassMethodPractice3/MoreClassMethodPractice3/Program.cs
+++ MoreClassMethodPractice3/MoreClassMethodPractice3/Program.cs
@@ -11,8 +11,8 @@ namespace MoreClassMethodPractice3
             int int1 = Convert.ToInt16(Console.ReadLine());
             int newInt2 = 1;
             Console.WriteLine("OPTIONAL Please enter another integer");
-            string int2 = Console.ReadLine();
-            if (!string.IsNullOrWhiteSpace(int2))
+            string int2 = Console.ReadLine(); //have to keep this as a string to see if its null. then convert if they give an int
+            if (!string.IsNullOrWhiteSpace(int2)) //if they entered a value do the below. Else give the null which will refer to the default value
             {
                 newInt2 = Convert.ToInt16(int2);
             }
