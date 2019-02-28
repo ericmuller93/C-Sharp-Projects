4f70528e482a280445527a32c16f1101c4db31c6
emuller93 <ericmuller93@gmail.com>
2019-02-28 12:36:56 -0800

    updated

---
+++ MoreClassMethodPractice2/MoreClassMethodPractice2/MyClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreClassMethodPractice2
+{
+    class MyClass
+    {
+        public int myMethod(int Int1, out int result)
+        {
+            result = Int1 * 2;
+            return result;
+        }
+
+        public decimal myMethod(decimal Int2, out int result2)
+        {
+            decimal answer = Int2 * 100;
+            result2 = Convert.ToInt16(answer);
+            return result2;
+        }
+
+        public int myMethod(string Int3, out int result3)
+        {
+            int answer = Convert.ToInt16(Int3);
+            result3 = answer + 111;
+            return result3;
+        }
+    }
+}
---
+++ MoreClassMethodPractice2/MoreClassMethodPractice2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreClassMethodPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an Int!");
+            int Int1 = Convert.ToInt16(Console.ReadLine());
+            MyClass firstClass = new MyClass();
+            firstClass.myMethod(Int1, out int result);
+            Console.WriteLine(result + " is the result of your int in my first Method");
+            Console.WriteLine("Please enter another Int!");
+            decimal Int2 = Convert.ToDecimal(Console.ReadLine());
+            firstClass.myMethod(Int2, out int result2);
+            Console.WriteLine(result2 + " is the result of your int in my second Method");
+            Console.WriteLine("Please enter another Int!");
+            string Int3 = Console.ReadLine();
+            firstClass.myMethod(Int3, out int result3);
+            Console.WriteLine(result3 + " is the result of your int in my third Method");
+            Console.ReadLine();
+        }
+    }
+}
