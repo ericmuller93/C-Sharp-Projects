0061609aa769bde4395a695ca116c4504a6b4f2f
emuller93 <ericmuller93@gmail.com>
2019-02-28 13:46:55 -0800

    adding new project

---
+++ MoreClassMethodPractice3/MoreClassMethodPractice3/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreClassMethodPractice3
+{
+    class Class1
+    {
+        public int myMethod(int int1, int newInt2 = 1)
+        {
+           int int3 = int1 + newInt2;
+           return int3;
+        }
+    }
+}
---
+++ MoreClassMethodPractice3/MoreClassMethodPractice3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreClassMethodPractice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer");
+            int int1 = Convert.ToInt16(Console.ReadLine());
+            int newInt2 = 1;
+            Console.WriteLine("OPTIONAL Please enter another integer");
+            string int2 = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(int2))
+            {
+                
+            }
+            else
+            {
+                newInt2 = Convert.ToInt16(int2);
+            }
+            Class1 myClass = new Class1();
+            Console.WriteLine("Your inputs in the method equal " + myClass.myMethod(int1, newInt2));
+            Console.ReadLine();
+
+        }
+    }
+}
