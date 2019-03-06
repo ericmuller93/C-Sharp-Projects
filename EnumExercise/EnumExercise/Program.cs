27251ec82a2bd4f1d48dacf30b723f6dab52e09b
emuller93 <ericmuller93@gmail.com>
2019-03-06 15:06:24 -0800

    commit

--- EnumExercise/EnumExercise/Program.cs
+++ EnumExercise/EnumExercise/Program.cs
@@ -14,18 +14,18 @@ namespace EnumExercise
             {
                 Console.WriteLine("Please enter a day of the week! (lowercase)");
                 string userDay = Console.ReadLine();
-                bool isNum = Int32.TryParse(userDay, out int number);
-                if (isNum == false)
+                bool isNum = Int32.TryParse(userDay, out int number); //this is checking if a number was entered. if so it returns true
+                if (isNum == false) //we only want false (no numbers). first if will move on with program
                 {
-                    Console.WriteLine(Enum.Parse(typeof(DaysOfTheWeek), userDay));
+                    Console.WriteLine(Enum.Parse(typeof(DaysOfTheWeek), userDay)); //checking if user input perfectly matches values in our enum. Try catch will pick up if they enter something thats not a match.
                 }
-                if (isNum)
+                if (isNum) //if they entered a number tell them no numbers
                 {
                     Console.WriteLine("Please don't enter numbers!");
                 }
                 
             }
-            catch
+            catch //catch will pickup if they entered letters that dont match with our days in the enum
             {
                 Console.WriteLine("Please enter an actual day of the week in lowercase!");
             }
